63f612155ad16d1da23eb6f57ff0fd5e22d270eb
h <87516482+linguithe@users.noreply.github.com>
2022-08-12 03:56:32 +0800

    updated the damn code finally

--- outlier.py
+++ outlier.py
@@ -4,43 +4,31 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from IPython.display import display
 
-#To observe outliers with boxplot
-def outlier_bp(df, colname):
-    column = df[colname]
-    sns.boxplot(y=column)
-    plt.show()
-
-#To observe outliers with interquartile range
-def outlier_iqr(df, colname):
-    tmp = pd.DataFrame()
-    first_quartile = np.percentile(df[colname], 25, method="midpoint")
-    third_quartile = np.percentile(df[colname], 75, method="midpoint")
-    iqr = third_quartile - first_quartile
-
-    upper_bound = np.where(df[colname] >= third_quartile + 1.5 * iqr)
-    lower_bound = np.where(df[colname] <= first_quartile - 1.5 * iqr)
-    i = 0
-    while i < len(upper_bound):
-        tmp = pd.concat([tmp, df.loc[upper_bound[i]]])
-        tmp = pd.concat([tmp, df.loc[lower_bound[i]]])
-        i += 1
-    display(tmp)
-
 #Read excel file.
 df = pd.read_excel("C:\\Users\\notth\\Documents\\MASA Hackathon\\Documentation & Materials\\TravelDataset.xlsx")
+df_cleaned= df.drop(df.index[df["Net Sales"] < 0])
 
-#Looking for outliers of age using boxplot and interquartile range.
-outlier_bp(df, "Age")
-outlier_iqr(df, "Age")
+#To observe outliers with boxplot
+column = df_cleaned["Age"]
+sns.boxplot(y=column)
+plt.show()
 
-#Looking for outliers of duration.
-outlier_bp(df, "Duration")
-outlier_iqr(df, "Duration")
+#To observe outliers with interquartile range
+#Write to excel for better viewing of full table
+tmp = pd.DataFrame()
+file_name = "AgeData.xlsx"
+first_quartile = np.percentile(df_cleaned["Age"], 25, method="midpoint")
+third_quartile = np.percentile(df_cleaned["Age"], 75, method="midpoint")
+iqr = third_quartile - first_quartile
 
-#Looking for outliers of net sales.
-outlier_bp(df, "Net Sales")
-outlier_iqr(df, "Net Sales")
+upper_bound = np.where(df_cleaned["Age"] >= third_quartile + 1.5 * iqr)
+lower_bound = np.where(df_cleaned["Age"] <= first_quartile - 1.5 * iqr)
+print(upper_bound)
+i = 0
+while i < len(upper_bound):
+    tmp = pd.concat([tmp, df_cleaned.iloc[upper_bound[i]]])
+    tmp = pd.concat([tmp, df_cleaned.iloc[lower_bound[i]]])
+    i += 1
 
-#Looking for outliers of commission.
-outlier_bp(df, "Commision (in value)")
-outlier_iqr(df, "Commision (in value)")
+display(tmp)
+tmp.to_excel(file_name)
