b7e016b7ea40283d45718a432aea1aa676cfeff9
h <87516482+linguithe@users.noreply.github.com>
2022-08-14 21:55:40 +0800

    more models (one output atm)

--- modelling.py
+++ modelling.py
@@ -1,9 +1,17 @@
+from audioop import cross
 import pandas as pd
 import joblib
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
 
 #Importing dataset and cleaning data.
 df = pd.read_excel("C:\\Users\\notth\\Documents\\MASA Hackathon\Documentation & Materials\\TravelDataset.xlsx")
@@ -22,8 +30,34 @@ le = LabelEncoder()
 y_train = le.fit_transform(y_train)
 y_test = le.transform(y_test)
 
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-predict = model.predict(X_test)
+#Decision tree with output claim.
+dtc = DecisionTreeClassifier()
+dtc.fit(X_train, y_train)
+predict = dtc.predict(X_test)
 accuracy = accuracy_score(y_test, predict)
 print(accuracy)
+
+#SGD
+sgd = SGDClassifier()
+sgd.fit(X_train, y_train)
+predict = sgd.predict(X_test)
+accuracy = accuracy_score(y_test, predict)
+print(accuracy)
+
+#Random forest
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+predict = rf.predict(X_test)
+accuracy = accuracy_score(y_test, predict)
+cv = cross_val_score(rf, X_train, y_train, cv=5)
+score = rf.score(X_test, y_test)
+print(accuracy, cv.mean(), score)
+
+#SVC
+svc = SVC(probability=True, C = 1, kernel="linear")
+svc.fit(X_train, y_train)
+predict = svc.predict(X_test)
+accuracy = accuracy_score(y_test, predict)
+cv = cross_val_score(rf, X_train, y_train, cv=5)
+score = svc.score(X_test, y_test)
+print(accuracy, cv.mean(), score)
